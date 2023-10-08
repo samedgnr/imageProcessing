562823f8d474b076e8a96074b1c1da2fbf44c91f
samedgngr <samedgng@gmail.com>
2023-10-08 16:55:33 +0300

    kod duzenlendi

--- main.py
+++ main.py
@@ -17,23 +17,27 @@ backends = [
 while True:
     ret, frame = cap.read()
     try:
-        result = DeepFace.analyze(frame, actions=['emotion'], detector_backend="opencv")
+        if frame is None:
+            print("none")
+            continue
+        result = DeepFace.analyze(frame, actions=['emotion'])
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        cv2.putText(frame,
+                    result[0]["dominant_emotion"][:],
+                    (50, 50),
+                    font, 3,
+                    (0, 0, 255),
+                    2,
+                    cv2.LINE_4)
+
+        cv2.imshow('Original Video', frame)
+        cv2.waitKey(5000)
+        print(result[0]["emotion"])
+
+        if result[0]["dominant_emotion"][:]:
+            cap.release()
+            cv2.destroyAllWindows()
+            break
     except ValueError:
         pass
-
-    font = cv2.FONT_HERSHEY_SIMPLEX
-
-    cv2.putText(frame,
-                result[0]["dominant_emotion"][:],
-                (50, 50),
-                font, 3,
-                (0, 0, 255),
-                2,
-                cv2.LINE_4)
-    cv2.imshow('Original Video', frame)
-
-    if cv2.waitKey(2) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
